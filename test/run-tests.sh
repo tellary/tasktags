34afaeb68e6ab582b920d4e021b41f4430e34f50
Ilya Silvestrov <tellary@gmail.com>
2020-02-13 10:02:39 -0800

    Removed Elisp code to generate Toggl CSV

--- test/run-tests.sh
+++
@@ -1,7 +0,0 @@
-emacs -batch \
-      -l ../stream.el \
-      -l stream-test.el \
-      -f package-initialize \
-      -l ../task-tags-mode.el \
-      -l task-tags-test.el \
-      -f ert-run-tests-batch-and-exit
