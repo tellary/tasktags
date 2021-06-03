24d56d55f595e018f30a7b6f1b72497896bfce5e
Ilya Silvestrov <tellary@gmail.com>
2021-06-02 21:43:31 -0700

    Implement `createTimeEntry`

--- TogglAPI.hs
+++ TogglAPI.hs
@@ -4,24 +4,28 @@
 module TogglAPI where
 
 import           Control.Exception     (catch, throw)
-import           Control.Lens          ((&), (.~), (?~), (^.), (^?))
+import           Control.Lens          ((&), (?~), (^?))
 import           Control.Monad         (when)
-import           Data.Aeson            (FromJSON, ToJSON,
-                                        Value (Number, Object, String))
-import           Data.Aeson.Lens       (key, _JSON, _Number, _String)
+import           Data.Aeson            (FromJSON, ToJSON (toJSON),
+                                        Value (Object), object, (.=))
+import           Data.Aeson.Lens       (key, _JSON, _Null, _Number)
+import           Data.Aeson.Types      (Pair)
 import qualified Data.ByteString.Char8 as C
-import           Data.HashMap.Strict   (fromList)
+import           Data.HashMap.Strict   (fromList, union)
 import           Data.Text             as T
-import           Data.Time             (ZonedTime)
+import           Data.Time             (ZonedTime, diffUTCTime,
+                                        nominalDiffTimeToSeconds,
+                                        zonedTimeToUTC)
 import           GHC.Generics          (Generic)
 import           Network.HTTP.Client   (HttpException (HttpExceptionRequest),
                                         HttpExceptionContent (
                                            StatusCodeException),
                                         Response (responseStatus))
 import           Network.HTTP.Types    (Status (statusCode))
-import           Network.Wreq          (asJSON, auth, basicAuth, defaults,
-                                        getWith, param, postWith, responseBody)
+import           Network.Wreq          (auth, basicAuth, defaults, getWith,
+                                        postWith, responseBody)
 import           Text.Printf           (printf)
+import           ZonedTime             ()
 
 data TimeEntry
   = TimeEntry
@@ -29,7 +33,9 @@ data TimeEntry
   , pid :: Int
   , start :: ZonedTime
   , stop :: ZonedTime
-  } deriving Show
+  } deriving (Show, Generic)
+
+instance ToJSON TimeEntry
 
 data Project
   = Project
@@ -38,7 +44,7 @@ data Project
   } deriving (Show, Generic)
 
 instance FromJSON Project
-instance ToJSON Project
+instance ToJSON   Project
 
 type Pid = Int
 type ProjectName = String
@@ -62,24 +68,13 @@ instance TogglAPI TogglAPIImpl where
     let opts
           = defaults
           & auth ?~ basicAuth (C.pack apiKey) "api_token"
-          & param "user_agent" .~ ["task-tags"]
     let url
           =  "https://" ++ host impl
           ++ maybe "" (\p -> ":" ++ show p) (port impl)
           ++ "/api/v8/projects"
     let body
-          = Object
-          ( fromList
-            [ ("project"
-              , Object
-                ( fromList
-                  [ ("name", String . T.pack $ name)
-                  , ("wid" , Number . fromIntegral $ wid)
-                  ]
-                )
-              )
-            ]
-          )
+          = object
+          [ "project" .= object [ "name" .= T.pack name, "wid" .= wid ] ]
     r <- postWith opts url body
       `catch`
       \e@(HttpExceptionRequest _ (StatusCodeException r msg)) -> do
@@ -92,12 +87,37 @@ instance TogglAPI TogglAPIImpl where
       Just id -> return . round $ id
       Nothing -> error "Failed to parse project id in creation response"
 
+  -- https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md#create-a-time-entry
+  createTimeEntry impl apiKey entry = do
+    let opts
+          = defaults
+          & auth ?~ basicAuth (C.pack apiKey) "api_token"
+    let url
+          =  "https://" ++ host impl
+          ++ maybe "" (\p -> ":" ++ show p) (port impl)
+          ++ "/api/v8/time_entries"
+    let duration
+          = truncate . nominalDiffTimeToSeconds
+          $ diffUTCTime
+            (zonedTimeToUTC . stop  $ entry)
+            (zonedTimeToUTC . start $ entry)
+          :: Int
+    let body
+          = object
+          [ "time_entry"
+            .= entry `addFields`
+               [ "created_with" .= T.pack "task-tags"
+               , "duration"     .= duration
+               ]
+          ]
+    postWith opts url body
+    return ()
+
   -- https://github.com/toggl/toggl_api_docs/blob/master/chapters/workspaces.md#get-workspace-projects
   listWorkspaceProjects impl apiKey wid = do
     let opts
           = defaults
           & auth ?~ basicAuth (C.pack apiKey) "api_token"
-          & param "user_agent" .~ ["task-tags"]
     let url
           =  "https://" ++ host impl
           ++ maybe "" (\p -> ":" ++ show p) (port impl)
@@ -105,10 +125,15 @@ instance TogglAPI TogglAPIImpl where
     r <- getWith opts url
     case r ^? responseBody . _JSON of
       Just tes -> return tes
-      Nothing  -> do
-        if (r ^. responseBody . _String == "")
-          then return []
-          else error $ "Failed to parse response body as JSON:\n" ++ show r
+      Nothing  ->
+        case r ^? responseBody . _Null of
+          Just () -> return []
+          Nothing
+            -> error $ "Failed to parse response body as JSON:\n" ++ show r
+
+addFields :: ToJSON a => a -> [Pair] -> Value
+addFields a pairs = Object $ hm `union` fromList pairs
+  where Object hm = toJSON a
 
 togglAPI = TogglAPIImpl "api.track.toggl.com" Nothing
 
